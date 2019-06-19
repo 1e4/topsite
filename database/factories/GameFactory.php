495499f91cfbadc123cbcce41b213a344f432810
Ian <hi@ian.pe>
2019-06-19 18:28:03 +0100

    Fix order by rand, could be DB specific?
    Also remove duplicate category in factory

--- database/factories/GameFactory.php
+++ database/factories/GameFactory.php
@@ -12,8 +12,7 @@ $factory->define(\App\Game::class, function (Faker $faker) {
         'url'   =>  $faker->url,
         'description'   =>  $faker->realText($faker->numberBetween(30,60)),
         'is_premium'    =>  mt_rand(1,5) == 1 ? true : false,
-        'category_id'   =>  1,
         'is_pending'    =>  0,
-        'category_id'  =>  \App\Category::orderByRaw("RAND()")->limit(1)->first()->id,
+        'category_id'  =>  \App\Category::all()->random()->first()->id,
     ];
 });
