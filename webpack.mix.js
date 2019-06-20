b9e30b950bc8fed3cf806f5a8e61ed12e163e39c
Ian <hi@ian.pe>
2019-06-20 16:19:50 +0100

    Remove number format and put into helpers

--- webpack.mix.js
+++ webpack.mix.js
@@ -21,11 +21,14 @@ mix.copy('resources/js/admin/', 'public/js/admin/')
     .sass('resources/sass/admin/admin.scss', 'public/css/admin')
     .version();
 
+mix.copy('resources/js/helpers.js', 'public/js')
+    .version();
+
 // Jquery stuff
 mix.copy('node_modules/jquery/dist/jquery.min.js', 'public/js')
     .copy('node_modules/bootstrap/dist/js/bootstrap.bundle.min.js', 'public/js/admin')
-    .copy('node_modules/jquery.easing/jquery.easing.min.js', 'public/js/admin')
-    .copy('node_modules/chart.js/dist/Chart.min.js', 'public/js/admin')
+    .copy('node_modules/jquery.easing/jquery.easing.min.js', 'public/js')
+    .copy('node_modules/chart.js/dist/Chart.min.js', 'public/js')
     .copy('node_modules/datatables.net-bs4/js/dataTables.bootstrap4.min.js', 'public/js/admin')
     .copy('node_modules/datatables.net/js/jquery.dataTables.min.js', 'public/js/admin')
     .version();
