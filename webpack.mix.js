d22289207a7c7ee5187b1475228c60ff11994e27
Ian <hi@ian.pe>
2019-06-28 16:49:30 +0100

    add dropzonejs

--- webpack.mix.js
+++ webpack.mix.js
@@ -31,4 +31,5 @@ mix.copy('node_modules/jquery/dist/jquery.min.js', 'public/js')
     .copy('node_modules/chart.js/dist/Chart.min.js', 'public/js')
     .copy('node_modules/datatables.net-bs4/js/dataTables.bootstrap4.min.js', 'public/js/admin')
     .copy('node_modules/datatables.net/js/jquery.dataTables.min.js', 'public/js/admin')
+    .copy('node_modules/dropzone/dist/min/dropzone.min.js', 'public/js')
     .version();
