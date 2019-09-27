73a9690a015f065c51f29e6962dd2a28eea2edb3
Ian <hi@ian.pe>
2019-09-27 20:49:35 +0100

    Add categories into the sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,44 +19,46 @@
 <body>
 <div id="app">
     <div class="container">
-        <div class="col-12">
-            <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-                <div class="container">
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'PBBG Topsite') }}
-                    </a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse"
-                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                            aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
+        <div class="row">
 
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <!-- Left Side Of Navbar -->
-                        <ul class="navbar-nav mr-auto">
+            <div class="col-12">
+                <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+                    <div class="container">
+                        <a class="navbar-brand" href="{{ url('/') }}">
+                            {{ config('app.name', 'PBBG Topsite') }}
+                        </a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                                aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
 
-                        </ul>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <!-- Left Side Of Navbar -->
+                            <ul class="navbar-nav mr-auto">
 
-                        <!-- Right Side Of Navbar -->
-                        <ul class="navbar-nav ml-auto">
-                            <!-- Authentication Links -->
-                            @guest
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                                @if (Route::has('register'))
+                            </ul>
+
+                            <!-- Right Side Of Navbar -->
+                            <ul class="navbar-nav ml-auto">
+                                <!-- Authentication Links -->
+                                @guest
                                     <li class="nav-item">
-                                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                     </li>
-                                @endif
-                            @else
+                                    @if (Route::has('register'))
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                        </li>
+                                    @endif
+                                @else
 
-                                @if(Auth::user()->isAdmin())
-                                    <li class="nav-item">
-                                        <a class="nav-link"
-                                           href="{{ route('admin.home') }}">{{ __('Administration') }}</a>
-                                    </li>
-                                @endif
+                                    @if(Auth::user()->isAdmin())
+                                        <li class="nav-item">
+                                            <a class="nav-link"
+                                               href="{{ route('admin.home') }}">{{ __('Administration') }}</a>
+                                        </li>
+                                    @endif
 
                                     <li class="nav-item">
                                         <a class="nav-link"
@@ -66,48 +68,65 @@
                                         <a class="nav-link"
                                            href="{{ route('front.game.index') }}">{{ __('Manage Games') }}</a>
                                     </li>
-                                <li class="nav-item dropdown">
-                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                        {{ Auth::user()->name }} <span class="caret"></span>
-                                    </a>
-
-                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item" href="{{ route('account.index') }}">
-                                            {{ __('Control Panel') }}
+                                    <li class="nav-item dropdown">
+                                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                            {{ Auth::user()->name }} <span class="caret"></span>
                                         </a>
-                                        <a class="dropdown-item" href="{{ route('logout') }}"
-                                           onclick="event.preventDefault();
+
+                                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                            <a class="dropdown-item" href="{{ route('account.index') }}">
+                                                {{ __('Control Panel') }}
+                                            </a>
+                                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                               onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                            {{ __('Logout') }}
-                                        </a>
+                                                {{ __('Logout') }}
+                                            </a>
 
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                              style="display: none;">
-                                            @csrf
-                                        </form>
-                                    </div>
-                                </li>
-                            @endguest
-                        </ul>
+                                            <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                                  style="display: none;">
+                                                @csrf
+                                            </form>
+                                        </div>
+                                    </li>
+                                @endguest
+                            </ul>
+                        </div>
                     </div>
-                </div>
-            </nav>
+                </nav>
+            </div>
         </div>
     </div>
     <div class="container">
-        <div class="col-12">
-            <main class="py-4">
-                @if(auth()->user())
-                    @if(!auth()->user()->email_verified_at && request()->route()->getName() !== 'verification.notice')
-                        <div class="alert alert-danger">
-                            <a href="{{ route('verification.notice') }}">Your account has not been activated yet, you cannot add any listings or comment/rate until this is done, click here to verify.</a>
+        <div class="row">
+            <div class="col-3 py-4">
+                <div class="card">
+
+                    <div class="card-body">
+                        <div class="nav flex-column">
+                            @foreach($categories as $category)
+                                <a href="{{ route('front.category.show', $category->slug) }}">{{ $category->name }}</a>
+                            @endforeach
                         </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-9">
+                <main class="py-4">
+                    @if(auth()->user())
+                        @if(!auth()->user()->email_verified_at && request()->route()->getName() !== 'verification.notice')
+                            <div class="alert alert-danger">
+                                <a href="{{ route('verification.notice') }}">Your account has not been activated yet,
+                                    you cannot add any listings or comment/rate until this is done, click here to
+                                    verify.</a>
+                            </div>
+                        @endif
                     @endif
-                @endif
-                @include('flash::message')
-                @yield('content')
-            </main>
+                    @include('flash::message')
+                    @yield('content')
+                </main>
+            </div>
         </div>
     </div>
 </div>
