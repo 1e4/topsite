710f4790f842dc5364ee64406b71500d69c18f17
Ian <hi@ian.pe>
2019-09-27 21:04:15 +0100

    Show categories

--- app/Http/Controllers/ListingCategoryController.php
+++ app/Http/Controllers/ListingCategoryController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
+use App\Game;
 use Illuminate\Http\Request;
 
 class ListingCategoryController extends Controller
 {
-    //
+    public function show($category)
+    {
+
+        $currentCategory = Category::whereSlug($category)->first();
+
+        $listings = Game::where('category_id', $currentCategory->id)
+            ->where('is_pending', false)
+            ->paginate(50);
+
+        return view('home', compact('currentCategory', 'listings'));
+    }
 }
