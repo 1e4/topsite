1d994962de2d1545c3b21b8af5cc0a13fac6a7e5
Ian <hi@ian.pe>
2019-10-10 22:12:11 +0100

    Solve #31 add vote snippet

--- resources/views/games/edit.blade.php
+++ resources/views/games/edit.blade.php
@@ -3,6 +3,29 @@
 @section('content')
     @include('layouts.games-tabs')
 
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Vote Snippet For {{ $game->name }}</h6>
+        </div>
+        <div class="card-body">
+
+            <p>
+                Use the snippet below to add a vote link to your website
+            </p>
+
+            <div class="mb-4">
+                <a href="{{ route('listing.in', [$game->slug]) }}" target="_blank" rel="nofollow">
+                    <img src="{{ asset('images/theme/vote_for_button.jpg') }}" />
+                </a>
+            </div>
+
+            <pre class="mb-0">
+{!! htmlentities("<a href='". route('listing.in', [$game->slug])."' target='_blank' rel='nofollow'>
+    <img src=". asset('images/theme/vote_for_button.jpg') ." />
+</a>") !!}
+            </pre>
+        </div>
+    </div>
 
 
     <div class="card shadow mb-4">
