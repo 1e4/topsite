bfaa190392c8941aad201d9bc063db831eab8753
Ian <hi@ian.pe>
2019-10-10 17:57:02 +0100

    Add out link for outgoing links on listing show

--- resources/views/listing/show.blade.php
+++ resources/views/listing/show.blade.php
@@ -10,7 +10,7 @@
                     </div>
                 @endif
                 <h2>{{ $game->name }} @if(auth()->user() && $game->created_by === auth()->user()->id) <span class="float-right"><a href="{{ route('front.game.edit', $game) }}" class="btn btn-info">Edit</a> </span> @endif</h2>
-                <h4 class="text-muted"><a href="{{ $game->url }}">{{ $game->url }}</a></h4>
+                <h4 class="text-muted"><a href="{{ route('listing.out', $game->slug) }}">{{ $game->url }}</a></h4>
             </div>
 
             <hr>
